2e9373c005441199944d4d815f385615e2e600ad
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2020-07-09 11:05:25 +0400

    fix: в файле js необходимо убрать повторение кода одинаковыми функциями, также исправить input кнопки отправки на button с type=submit и реализовать код таким образом, чтобы при изменении данных в popup и закрытии без сохранения данные не изменялись в текстовых полях

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
-let popupButtonOpen = document.querySelector('.profile__edit-button');
-let popupButtonClose = document.querySelector('.popup__close-icon');
-let popup = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup__form');
-let formInputName = document.querySelector('.popup__name');
-let formInputJob = document.querySelector('.popup__job');
-let popupSubmitClose = document.querySelector('.popup__submit');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const popupButtonOpen = document.querySelector('.profile__edit-button');
+const popupButtonClose = document.querySelector('.popup__close-icon');
+const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__form');
+const formInputName = document.querySelector('.popup__input_name');
+const formInputJob = document.querySelector('.popup__input_job');
+const popupSubmitClose = document.querySelector('.popup__submit');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
-popupButtonClose.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
+function togglePopup() {
+  popup.classList.toggle('popup_opened');
+}
+
+popupButtonOpen.addEventListener('click', togglePopup);
+popupButtonClose.addEventListener('click', togglePopup);
+popupSubmitClose.addEventListener('click', togglePopup);
+
+popupForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
   formInputName.value.textContent = profileTitle;
   formInputJob.value.textContent = profileSubtitle;
-});
-
-popupButtonOpen.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
-});
-
-popupForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
 
   profileTitle.textContent = formInputName.value;
   profileSubtitle.textContent = formInputJob.value;
 });
-
-popupSubmitClose.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
-})
