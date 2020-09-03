b51b8ee504748bf9568543e037df3be054b6675a
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2020-09-03 17:32:22 +0400

    refactor: сделать файл FormValidator.js и создать в нем класс js с валидацией форм, также експортировать его и файл Card.js в index.js, сделать запись в файл READMI.md

--- js/FormValidator.js
+++ js/FormValidator.js
@@ -5,7 +5,6 @@ class FormValidator {
 		  inactiveButton,
 		  errorMsg, 
 		  invalidInput
-		  //class names
 	}, formElem)
 	{
 		for (let key in arguments[0]) {
@@ -15,89 +14,68 @@ class FormValidator {
 		this.form = formElem;
 	}
 	
-	enableValidation = () => {
-      this.setEventListeners(); //вешает новые события
+	enableValidation() {
+    this._setEventListeners(); // вешает новые события
 	};
 	
-	setEventListeners = () => {
-	  this.inputList = Array.from(this.form.querySelectorAll(this.input)); //Выбирает все инпуты из формы
-	  this.button = this.form.querySelector(this.submit); //и кнопку из неё
-	  this.inputList.forEach((input) => {//для каждого инпута
-	  input.addEventListener('input', () => {//вешает валидацию?
-		  this.toggleInputState(input); //разбирается с тем, как выглядят инпуты и отображаются ли ошибки.
-		  this.toggleButtonState(); //разбирается с кнопкой: активна или неактивна?
+	_setEventListeners() {
+	  this.inputList = Array.from(this.form.querySelectorAll(this.input)); // Выбирает все инпуты из формы
+	  this.button = this.form.querySelector(this.submit); // и кнопку из неё
+	  this.inputList.forEach((input) => { // для каждого инпута
+	  input.addEventListener('input', () => { // вешает валидацию?
+		  this._toggleInputState(input); // разбирается с тем, как выглядят инпуты и отображаются ли ошибки.
+		  this._toggleButtonState(); // разбирается с кнопкой: активна или неактивна?
 		});
 	  });
 	};
 	
-	hasInvalidInput = () => {
+	_hasInvalidInput() {
 	  return this.inputList.some((input) => {
 		return !input.validity.valid;
 	  })
 	};
 	
-	toggleInputState = (input) => { //бывшая isValid
+	_toggleInputState(input) { // бывшая isValid
 		if (!input.validity.valid) {
-			this.showInputError(input);
+			this._showInputError(input);
 		} else {
-			this.hideInputError(input);
+			this._hideInputError(input);
 		}
 	}
 	
-	showInputError = (input) => {
-		const errorElem = this.form.querySelector(`#${input.id}-error`); //нашли куда класть ошибку
-		console.log('error element:', errorElem);
-		input.classList.add(this.invalidInput.slice(1)); //объявили инпут ошибочным
-		errorElem.textContent = input.validationMessage; //в элемент с ошибкой записали текст ошибки
-		console.log('message:', input.validationMessage);
-		errorElem.classList.add(this.errorMsg.slice(1)); // добавили элементу с ошибкой класс ошибки
+	_showInputError(input) {
+		const errorElem = this.form.querySelector(`#${input.id}-error`); // нашли куда класть ошибку
+		input.classList.add(this.invalidInput); // объявили инпут ошибочным
+		errorElem.textContent = input.validationMessage; // в элемент с ошибкой записали текст ошибки
+		errorElem.classList.add(this.errorMsg); // добавили элементу с ошибкой класс ошибки
 	};
 	
-	hideInputError = (input) => {
+	_hideInputError(input) {
 		const errorElem = this.form.querySelector(`#${input.id}-error`);//нашли куда клали ошибку
 
-		input.classList.remove(this.invalidInput.slice(1)); //убрали инпут из класса ошибочных
-		errorElem.classList.remove(this.errorMsg.slice(1));  //элемент, где лежала ошибка, тоже убрали из класса ошибок
-		errorElem.textContent = '';			  //и убрали из него текст
+		input.classList.remove(this.invalidInput); // убрали инпут из класса ошибочных
+		errorElem.classList.remove(this.errorMsg);  // элемент, где лежала ошибка, тоже убрали из класса ошибок
+		errorElem.textContent = '';			  // и убрали из него текст
 	};
 	
-	toggleButtonState = () => {
-		if (this.hasInvalidInput(this.inputList)) {
-			this.button.classList.add(this.inactiveButton.slice(1));
-			this.activeButtonsPopups();
+	_toggleButtonState() {
+		if (this._hasInvalidInput(this.inputList)) {
+			this.button.classList.add(this.inactiveButton);
+			this._activeButtonsPopups();
 			// если хотя бы один инпут в inputList с ошибкой, выключаем кнопку
 		} else {
-			this.button.classList.remove(this.inactiveButton.slice(1));
-			this.inactiveButtonsPopups();
-			//всё ок? включаем
+			this.button.classList.remove(this.inactiveButton);
+			this._inactiveButtonsPopups();
+			// всё ок? включаем
 		}
 	}
 	
-	activeButtonsPopups = () => {
+	_activeButtonsPopups() {
 		this.button.setAttribute('disabled', true);
 	}
-	inactiveButtonsPopups = () => {
+	_inactiveButtonsPopups() {
 		this.button.removeAttribute('disabled');
 	}
 };
 
 export default FormValidator;
-/*
-function foo() {};
-
-function foo2(arg){
-arg(4, 3);
-
-};
-
-foo2(foo); //передали функцию
-foo2(foo()); //передали результат работы функции
-
-function sum(a, b) {
-  return a + b;
-}
-
-foo2(sum);
-foo2(sum(3, 5));
-
-*/
--- js/index.js
+++ js/index.js
@@ -1,17 +1,4 @@
-// import { 
-//   FormValidator, 
-//   setEventListeners, 
-//   isValid, 
-//   showInputError, 
-//   hideInputError, 
-//   hasInvalidInput, 
-//   activeButtonsPopups, 
-//   inactiveButtonsPopups, 
-//   toggleButtonState
-// } from './FormValidator.js';
-
-// import { dataForms, FormValidator } from './FormValidator.js';
-
+import Card from './Card.js';
 import FormValidator from './FormValidator.js';
 
 //HTML block with data from section profile
@@ -21,7 +8,6 @@ const profileSubtitle = document.querySelector('.profile__subtitle');
 //Popups
 const editProfileModal = document.querySelector('.popup_type_edit-profile');
 const addCardModal = document.querySelector('.popup_type_add-cards');
-const imageModal = document.querySelector('.popup_type_img');
 
 //Forms
   //__popup edit
@@ -35,10 +21,9 @@ const formInputName = editForm.querySelector('.form__input_name');
     //_2-й input
 const formInputJob = editForm.querySelector('.form__input_job');
 
-  //__inputs add
-    //_1-й input
+// 1) первый input формы в модальном окне Add
+// 2) второй input формы в модальном окне Add
 const formInputPlace = addForm.querySelector('.form__input_place');
-    //_2-й input
 const formInputUrl = addForm.querySelector('.form__input_url');
 
 //Buttons
@@ -57,56 +42,45 @@ const submitAddCard = addCardModal.querySelector('.form__submit');
 const buttonCloseModal = editProfileModal.querySelector('.popup__close-icon');
     //_popup add
 const buttonClosePopupAdd = addCardModal.querySelector('.popup__close-icon');
-    //_popup img
-const buttonClosePopupImg = imageModal.querySelector('.popup__close-icon');
 
-//Popup img with data
-  //__title
-const imgModalTitle = imageModal.querySelector('.popup__title-img');
-  //__big image
-const imgModalImg = imageModal.querySelector('.popup__img');
-
-//Card items
-// нашел класс template_а в html разметке, далее нашел внутри его контента класс элемента и записал в константу
-const gridElementCard = document.querySelector('.grid__elements').content.querySelector('.element');
-  //__section grid for Card item
+// нашел в документе класс списка, в который будет помещена разметка из js и записал в константу
+// записал объект с новым содержимым для карточек в grid секцию в константу
 const gridElements = document.querySelector('.elements');
-  //__object with images
 const initialCards = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-]; 
+];
+
 const dataForms = {
   form: '.form',
   input: '.form__input',
   submit: '.form__submit',
-  inactiveButton: '.form__submit_active',
-  errorMsg: '.form__input-error_active',
-  invalidInput: '.form__input_type_error'
+  inactiveButton: 'form__submit_active',
+  errorMsg: 'form__input-error_active',
+  invalidInput: 'form__input_type_error'
 }
-
   
  //data from section profile in fieldset form edit and add toggle for open and close popup
 function substitutingDataInInputFormEdit() { 
@@ -122,78 +96,31 @@ function handlerSubmitForm(evt) {
   closeModal(editProfileModal);
 }
 
-//add new card
-function handlerSubmitAddCard(evt) {
-  evt.preventDefault();
-  renderCard({name: formInputPlace.value, link: formInputUrl.value});
-  formInputPlace.value = '';
-  formInputUrl.value = '';
-  //activeButtonsPopups(submitAddCard, 'form__submit_active');
-  closeModal(addCardModal);
-}
-
-initialCards.forEach((element) => {
-  renderCard(element);
+// 1) функция обходит объект и получает его свойства
+// 2) вызывает функцию и передает в неё свойства
+initialCards.forEach(({name, link}) => {
+  renderCard(name, link);
 });
 
-function renderCard(element) {
-  gridElements.prepend(createCard(element));
+// 1) в функцию кладу свойства объекта для дальнейшего рендера новой карточки в grid
+// 2) возвращает новый объект с необходимыми свойствами при помощью new и записывет в константу card
+// 3) берет константу card с данными объекта и добавляет к ней новую сгенерированную карточку из класса Card
+// 4) в блок grid из html-DOM, в который необходимо вставить карточки добавляет в начало списка сгенерированную в методе generateCard
+function renderCard(name, link) {
+  const card = new Card(name, link);
+  const cardElement = card.generateCard();
+
+  gridElements.prepend(cardElement);
 }
 
-//add cards width images, titles, likes and delete buttons by page
-function createCard(element) {
-  //clone template
-  const elementCard = gridElementCard.cloneNode(true);
-  //title из template
-  const elementCardTitle = elementCard.querySelector('.element__title');
-  //img из template
-  const elementCardImg = elementCard.querySelector('.element__img');
-  //like button из template
-  const likeButtonElementCard = elementCard.querySelector('.element__button-like');
-  //delete button из template
-  const deleteElementCardButton = elementCard.querySelector('.element__button-delete');
-
-  //listen cards, open popup and enters data
-  elementCardImg.addEventListener('click', () => { 
-    openModal(imageModal);
-    fillCardBigImg(elementCardTitle, elementCardImg, elementCardTitle);
-  });
-
-  //listen cards, like card in grid Img
-  likeButtonElementCard.addEventListener('click', () => {
-    likeCard(likeButtonElementCard);
-  });
-
-  //listen cards, delete card in grid Img
-  deleteElementCardButton.addEventListener('click', () => {
-    deleteCard(deleteElementCardButton);
-  });
-
-  //fill data card
-  elementCardTitle.textContent = element.name;
-  elementCardImg.src = element.link;
-  elementCardImg.alt = element.name;
-
-  return elementCard;
-}
-
-//fill data card big img
-const fillCardBigImg = function(title, img, alt) {
-  imgModalTitle.textContent = title.textContent;
-  imgModalImg.src = img.src;
-  imgModalImg.alt = alt.textContent;
-}
-
-//like card in grid Img
-const likeCard = function(like) {
-  like.classList.toggle('element__button-like_focus');
-}
-
-//delete card in grid Img
-const deleteCard = function(element) {
-  const listItem = element.closest('.element');
-  listItem.remove();
-}
+// функция добавления новой карточки в grid(сбрасывает дефолтное поведенин кнопки)
+function handlerSubmitAddCard(evt) {
+  evt.preventDefault();
+  renderCard(formInputPlace.value, formInputUrl.value);
+  formInputPlace.value = '';
+  formInputUrl.value = '';
+  closeModal(addCardModal);
+};
 
 // 1) добавляет класс открытия модалки необходимой модалке
 // 2) слушает документ и при клике на клавиатуру вызывает функцию
@@ -204,9 +131,9 @@ function openModal(popup) {
   popup.addEventListener('click', closePopupsByOverlay);
 }
 
-// 1)удаляет класс для закрытия модального окна
-// 2)удаляет из документа слушатель по клику на клавиатуру и вызывает функцию
-// 3)удаляет по клику из той модалки, которую мы подставим слушатель и вызывает функцию
+// 1) удаляет класс для закрытия модального окна
+// 2) удаляет из документа слушатель по клику на клавиатуру и вызывает функцию
+// 3) удаляет по клику из той модалки, которую мы подставим слушатель и вызывает функцию
 function closeModal(popup) {
   popup.classList.remove('popup_opened');
   document.removeEventListener('keydown', closePopupByEscape);
@@ -255,11 +182,7 @@ buttonClosePopupAdd.addEventListener('click', () => {
   closeModal(addCardModal);
 });
 
-//слушает кнопку-крестик закрытия модалки Img и при клике вызывает функцию с определенной модалкой
-buttonClosePopupImg.addEventListener('click', () => {
-  closeModal(imageModal);
-});
-
+// находит все наши формы и вызывает метод валидации
 document.querySelectorAll(dataForms.form).forEach(form => {
 	new FormValidator(dataForms, form).enableValidation();
 })
