8cd5ccc2bfc7048b240f92f2b2b415b47ec8cd63
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-02-07 18:34:58 +0300

    feat: добавить папку dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path'); // подключаем path к конфигу вебпак
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,13 +8,14 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: '',
+    publicPath: ''
   },
   mode: 'development',
   devServer: {
-    contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-    open: true, // сайт будет открываться сам при запуске npm run dev
+    contentBase: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true,
   },
+  plugins: [new CleanWebpackPlugin()],
 };
