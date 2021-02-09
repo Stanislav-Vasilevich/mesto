b4a6bc8a454acc7ea80a27eb3d665901c548572c
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-02-09 09:06:35 +0300

    fix: delete parameters from class PopupWithImage.js and add their in method this class - open()

--- src/js/components/PopupWithImage.js
+++ src/js/components/PopupWithImage.js
@@ -1,18 +1,14 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
-  constructor( { src, alt }, popupSelector ) {
+  constructor( popupSelector ) {
     super( popupSelector );
-    this._src = src;
-    this._alt = alt;
   }
 
-  open = () => {
-    this._elem.classList.add('popup_opened');
-    this._img = this._elem.querySelector('.popup__img');
-    this._title = this._elem.querySelector('.popup__title-img');
-    this._img.src = this._src;
-    this._img.alt = this._alt;
-    this._title.textContent = this._alt;
+  open = (link, name) => {
+    super.open();
+    this._img.src = link;
+    this._img.alt = name;
+    this._title.textContent = name;
   }
 }
