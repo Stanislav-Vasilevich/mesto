e490e9f36a5429349e565153aca5496a26469a6c
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-02-09 09:11:56 +0300

    fix: add elements image and title in constructor class PopupWithImage.js

--- src/js/components/PopupWithImage.js
+++ src/js/components/PopupWithImage.js
@@ -3,6 +3,8 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
   constructor( popupSelector ) {
     super( popupSelector );
+    this._img = this._elem.querySelector('.popup__img');
+    this._title = this._elem.querySelector('.popup__title-img');
   }
 
   open = (link, name) => {
